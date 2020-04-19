ddcd7b142bd8f0857becc7f9db5e282a0030865e
Jacob Habtemariam <jacob@Jacobs-MBP.home>
2020-04-19 04:41:08 -0400

    set up axios call of weather API

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-require('dotenv').config();
+import './components/NavBar';
+import Axios from 'axios';
+import NavBar from './components/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-
+class App extends Component {
+  state = {  }
+  componentDidMount(){
+  this.getWeather();
+  }
+  getWeather = async() =>{
+    await Axios.get(`https://api.openweathermap.org/data/2.5/forecast?id=4366647&units=imperial&appid=${process.env.REACT_APP_API_KEY}`)
+    .then(res=>console.log(res));
+  }
+  render() { 
+    return ( 
+      <div className="App">
+      <NavBar>JEH Weather App</NavBar>
     </div>
-  );
+     );
+  }
 }
-
+ 
 export default App;
