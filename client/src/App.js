235224af687786a635dc9154fadde86a27a0cc84
Jacob Habtemariam <jacob@Jacobs-MBP.home>
2020-04-21 21:08:00 -0400

    pulled current forecast data. basic layout for current forecast

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,56 @@ import './App.css';
 import './components/NavBar';
 import Axios from 'axios';
 import NavBar from './components/NavBar';
+import WeatherDiv from './components/WeatherDiv';
+
 
 class App extends Component {
-  state = {  }
+  state = { city:'Reisterstown',
+            temp:undefined,
+            humidity:undefined,
+            windSpeed:undefined,
+            icon:undefined,
+            date:undefined            
+}
   componentDidMount(){
   this.getWeather();
   }
   getWeather = async() =>{
-    await Axios.get(`https://api.openweathermap.org/data/2.5/forecast?id=4366647&units=imperial&appid=${process.env.REACT_APP_API_KEY}`)
-    .then(res=>console.log(res));
+    await Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=reisterstown&units=imperial&appid=${process.env.REACT_APP_API_KEY}`)
+    .then(res=>{
+      console.log(res)
+      //Format current date
+      let rawDate = new Date((res.data.sys.sunrise) * 1000);
+                let d = rawDate.getDate();
+                let m = rawDate.getMonth() + 1;
+                let y = rawDate.getFullYear();
+                let currentDate = (m + '/' + d + '/' + y);
+      this.setState({
+        temp:parseInt(res.data.main.temp), 
+        humidity:res.data.main.humidity,
+        windSpeed:res.data.wind.speed,
+        icon:res.data.weather[0].icon, 
+        date:currentDate
+      })
+      
+      
+      console.log(this.state)
+   
+    });
   }
   render() { 
     return ( 
       <div className="App">
       <NavBar>JEH Weather App</NavBar>
+      <WeatherDiv city={this.state.city} 
+                  temp={this.state.temp}
+                  windSpeed={this.state.windSpeed}
+                  date={this.state.date}
+                  humidity={this.state.humidity}
+                  icon={this.state.icon}
+       />
+
+                  
     </div>
      );
   }
---
+++ client/src/components/WeatherDiv.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const WeatherDiv = (props) => {
+    return ( 
+        <div id='weatherDiv'>
+            <h1>{props.city} {props.date}</h1>
+            <ul id="currentForecast">
+                <li> <img alt='weather icon' src={'http://openweathermap.org/img/wn/' + props.icon + '@2x.png'}></img></li>
+                <li> <span id="temp">{props.temp}&deg;</span></li>
+                <li><h2>Humidity:{props.humidity}%</h2></li>
+                <li><h2>Wind: {props.windSpeed} mph</h2></li>
+            </ul> 
+        </div>
+     );
+}
+ 
+export default WeatherDiv;
